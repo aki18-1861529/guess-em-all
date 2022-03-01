87eec5f0780bae450fbf4c51567527a23abb2f1f
ari-apigo <ari.apigo125@gmail.com>
2022-02-28 23:41:12 -0800

    game ui (i think)

--- app/src/main/java/edu/us/ischool/guessemall/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/guessemall/MainActivity.kt
@@ -1,11 +1,19 @@
 package edu.us.ischool.guessemall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // start Game activity on Start Game button tap
+        findViewById<Button>(R.id.btnStartGame).setOnClickListener {
+            val intent = Intent(this, game::class.java)
+            startActivity(intent)
+        }
     }
 }
