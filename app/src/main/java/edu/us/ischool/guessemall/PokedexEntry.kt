5d300cf73ac4c8a54a042b5511d42a80d40a8692
lynnkn <lynnkn@uw.edu>
2022-03-02 17:51:06 -0800

    added gridview

---
+++ app/src/main/java/edu/us/ischool/guessemall/PokedexEntry.kt
@@ -0,0 +1,22 @@
+package edu.us.ischool.guessemall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class PokedexEntry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokedex_entry)
+
+        // getting intent from last activity
+        val pIntent = this.intent
+
+        // getting data
+        val name = pIntent.extras?.getString("EXTRA_NAME")
+
+        // setting page contents
+        val nameLabel: TextView = findViewById(R.id.name_label)
+        nameLabel.text = name
+    }
+}
